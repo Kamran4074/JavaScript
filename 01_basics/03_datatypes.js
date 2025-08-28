04bbed72be669836b5d89b688b29bc9c2ae0e16a
kamran alam <kammykamran0093@gmail.com>
2025-08-29 01:46:58 +0530

    conversion and datatypes

---
+++ 01_basics/03_datatypes.js
@@ -0,0 +1,17 @@
+// preemtive 
+// number ,string,boolean,bigint,null,undefined,symbol
+let a = 10;
+let b="Hello"; // string
+let c=true; // boolean  
+let d=BigInt(1234567898765432345678905433456787654345); // bigint
+let e=null; // null 
+let f=undefined; // undefined
+let g=Symbol("id"); // symbol
+
+
+// non preemtive
+// object,array,function
+let h={name:"John", age:30}; // object
+let i=[1,2,3,4,5]; // array 
+let j=function(){console.log("Hello World")}; // function
+console.log(d); 
